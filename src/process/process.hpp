a4d6e0e76e580da83eabc1b17faf90e8069babec
RafaBonach <rafa.bonach@gmail.com>
2026-06-24 23:33:54 -0300

    add parameters to PCB

--- src/process/process.hpp
+++ src/process/process.hpp
@@ -11,6 +11,22 @@ struct ProcessData {
     bool requiresScanner;
     bool requiresModem;
     bool requiresSata;
+
+    // Variavel de controle para o escalonador
+    int queueLevel = 0;
+    bool realTime;
+
+    int executedTime = 0; // Tempo de CPU já utilizado
+};
+
+class ProcessManipulator {
+    public:
+        static void aging(ProcessData& process) {
+            // Reduz a prioridade do processo para evitar starvation
+            if (process.priority > 0) {
+                process.priority--;
+            }
+        }
 };
 
 #endif
